a02b376df552de9ee183ef7f64d4dbe5f0980820
Данил Михайлов <mylongcode@gmail.com>
2025-12-30 21:52:18 +0500

    добавил пример использования паттерна

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var availableFactories = GetAvailableFactories();
+            Console.WriteLine("Available UI types");
+            foreach (var factory in availableFactories)
+                Console.WriteLine(factory);
+            var uiFactory = GetUIFactory("Windows");
+            var button = uiFactory.GetButton("Click me!");
+            var checkbox = uiFactory.GetCheckbox();
+            button.Click();
+            checkbox.Click();
+        }
+
+        static List<string> GetAvailableFactories()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var factories = assembly.GetTypes()
+                .Where(type => type.IsSubclassOf(typeof(UIFactory)) && !type.IsAbstract)
+                .ToList();
+            return factories.Select(type => type.Name.Replace("Factory", "")).ToList();
+        }
+
+        static UIFactory GetUIFactory(string uiFactoryType)
+        {
+            var factoryName = $"AbstractFactory.Factories.{uiFactoryType}Factory";
+            var assembly = Assembly.GetExecutingAssembly();
+            Type factoryType = assembly.GetType(factoryName);
+            if (factoryType == null) throw new NotSupportedException($"Factory for {uiFactoryType} notification is not found");
+            return (UIFactory)Activator.CreateInstance(factoryType);
+        }
+    }
+}
